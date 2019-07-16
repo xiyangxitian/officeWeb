8cba69ba0bb754c8f2322e3fa0e477b32da753a4
xiyangxitian <liyan@live.shop.edu.cn>
2019-07-16 19:26:41 +0800

    OFFICE 2

--- src/main/java/com/pdf/service/CreatePDFService.java
+++ src/main/java/com/pdf/service/CreatePDFService.java
@@ -1,8 +1,256 @@
 package com.pdf.service;
 
+import com.itextpdf.io.font.PdfEncodings;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Style;
+import com.itextpdf.layout.element.AreaBreak;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.property.AreaBreakType;
+import com.itextpdf.layout.property.TextAlignment;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.AcroFields;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfStamper;
+
+import java.io.*;
+
 public class CreatePDFService {
+    //
+    private Style wenhaoStyle;
+    private Style beizhuStyle;
+    private Style biaotiStyle;
+    //仿宋 正文用
+    private Style style;
+
+    public static CreatePDFService getInstance(){
+        CreatePDFService instance = new CreatePDFService();
+        try {
+            PdfFont wenhaoFont = PdfFontFactory.createFont("E:\\my\\pdf\\tool\\fzfs_GBK.ttf", PdfEncodings.IDENTITY_H,false);
+            PdfFont beizhuFont = PdfFontFactory.createFont("E:\\my\\pdf\\tool\\ht.ttf", PdfEncodings.IDENTITY_H,false);
+            PdfFont biaotiFont = PdfFontFactory.createFont("E:\\my\\pdf\\tool\\fzxbsjt.ttf", PdfEncodings.IDENTITY_H,false);
+            PdfFont font = PdfFontFactory.createFont("E:\\my\\pdf\\tool\\fs_GB2312.ttf", PdfEncodings.IDENTITY_H,false);
+            instance.wenhaoStyle = new Style();
+            instance.wenhaoStyle.setFont(wenhaoFont).setFontSize(16).setBold();
+            instance.beizhuStyle = new Style();
+            instance.beizhuStyle.setFont(beizhuFont).setFontSize(12).setBold();
+            instance.biaotiStyle = new Style();
+            instance.biaotiStyle.setFont(biaotiFont).setFontSize(22).setBold();
+            instance.style = new Style();
+            instance.style.setFont(font).setFontSize(12);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        CreatePDFService.getInstance().cratePdfByTemplate();
+/*        String templatePath = "E:\\my\\pdf\\cc.pdf";
+        try {
+            insertImage(templatePath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+    }
 
 
+    void createPdf(){
+        //创建PDF文档
+        Document document = new Document();
+
+        try {
+            FileOutputStream fos = new FileOutputStream("E:\\my\\aa.pdf");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void cratePdfByTemplate(){
+        try {
+            FileOutputStream fos = new FileOutputStream("E:\\my\\pdf\\outFile.pdf");
+            PdfReader pdfReader = new PdfReader("E:\\my\\pdf\\cc.pdf");
+
+            PdfWriter pdfWriter = new PdfWriter(fos);
+            //
+            PdfDocument pdfDocument = new PdfDocument(pdfReader, pdfWriter);
+
+            com.itextpdf.layout.Document doc = new com.itextpdf.layout.Document(pdfDocument, PageSize.A4);
+
+            doc.setMargins(0, 0, 50, 0);
 
 
+         /*   Paragraph p = getFirstLineIndentParagraph("1", 80, 80);
+            doc.add(p);
+            p = getFirstLineIndentParagraph("2", 80, 80);
+            doc.add(p);
+            p = getFirstLineIndentParagraph("3", 80, 80);
+            doc.add(p);
+            p = getFirstLineIndentParagraph("4", 80, 80);
+            doc.add(p);*/
+
+
+            Paragraph p = new Paragraph();
+            p.addStyle(biaotiStyle);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+
+//            doc.add(new AreaBreak(AreaBreakType.NEXT_AREA));
+
+
+            p = new Paragraph("报告文件头");
+            p.addStyle(biaotiStyle);
+            p.setTextAlignment(TextAlignment.CENTER);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+            doc.add(p);
+
+            doc.add(p);
+
+            doc.close();
+            pdfDocument.close();
+            pdfWriter.close();
+            pdfReader.close();
+            fos.close();
+
+        } catch (IOException e) {
+
+        }
+
+    }
+
+
+    private Paragraph getFirstLineIndentParagraph(String str,float marginLeft,float marginRight){
+        Paragraph p = new Paragraph(str);
+        p.addStyle(wenhaoStyle);
+        p.setFirstLineIndent(30);
+        p.setTextAlignment(TextAlignment.LEFT);
+        p.setMarginLeft(marginLeft);
+        p.setMarginRight(marginRight);
+        return p;
+    }
+
+    static void insertImage(String templatePath) throws Exception {
+        // 生成的文件路径
+        String targetPath = "E:\\my\\pdf\\target.pdf";
+        // 书签名
+        String fieldName = "BB";
+        // 图片路径
+        String imagePath = "C:\\Users\\ly\\Pictures\\1.jpg";
+
+        // 读取模板文件
+        InputStream input = new FileInputStream(new File(templatePath));
+//        PdfReader reader = new PdfReader(input);
+        com.itextpdf.text.pdf.PdfReader reader = new com.itextpdf.text.pdf.PdfReader(input);
+        PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(targetPath));
+        // 提取pdf中的表单
+        AcroFields form = stamper.getAcroFields();
+        form.addSubstitutionFont(BaseFont.createFont("STSong-Light","UniGB-UCS2-H", BaseFont.NOT_EMBEDDED));
+
+        // 通过域名获取所在页和坐标，左下角为起点
+/*        int pageNo = form.getFieldPositions(fieldName).get(0).page;
+        Rectangle signRect = form.getFieldPositions(fieldName).get(0).position;
+        float x = signRect.getLeft();
+        float y = signRect.getBottom();*/
+
+        // 读图片
+        Image image = Image.getInstance(imagePath);
+        // 获取操作的页面
+        PdfContentByte under = stamper.getOverContent(1);
+        // 根据域的大小缩放图片
+        //image.scaleToFit(signRect.getWidth(), signRect.getHeight());
+        // 添加图片
+        image.setAbsolutePosition(0, 0);
+        under.addImage(image);
+
+        stamper.close();
+        reader.close();
+    }
 }
---
+++ src/main/java/com/pdf/service/Word2PdfUtil.java
@@ -0,0 +1,73 @@
+package com.pdf.service;
+
+import com.aspose.words.Document;
+import com.aspose.words.License;
+import com.aspose.words.SaveFormat;
+import org.aspectj.weaver.ast.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+
+/**
+ * @author Administrator
+ * @version $Id$
+ * @since
+ * @see
+ */
+public class Word2PdfUtil {
+
+    public static void main(String[] args) {
+
+        //doc2pdf("E:\\my\\cc.doc", "E:\\my\\dd.pdf");
+
+       word2pdf("E:\\my\\pdf\\new.docx", "E:\\my\\pdf\\new1.pdf");
+    }
+
+    public static boolean getLicense() {
+        boolean result = false;
+        try {
+            InputStream is = Test.class.getClassLoader().getResourceAsStream("license.xml"); // license.xml应放在..\WebRoot\WEB-INF\classes路径下
+            License aposeLic = new License();
+
+            aposeLic.setLicense(is);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void doc2pdf(String inPath, String outPath) {
+        if (!getLicense()) { // 验证License 若不验证则转化出的pdf文档会有水印产生
+            return;
+        }
+        try {
+            long old = System.currentTimeMillis();
+            File file = new File(outPath); // 新建一个空白pdf文档
+            FileOutputStream os = new FileOutputStream(file);
+            Document doc = new Document(inPath); // Address是将要被转化的word文档
+            doc.save(os, SaveFormat.PDF);// 全面支持DOC, DOCX, OOXML, RTF HTML, OpenDocument, PDF,
+            // EPUB, XPS, SWF 相互转换
+            long now = System.currentTimeMillis();
+            System.out.println("共耗时：" + ((now - old) / 1000.0) + "秒"); // 转化用时
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void word2pdf(String docPath,String savePath){
+        try {
+            String s = "<License><Data><Products><Product>Aspose.Total for Java</Product><Product>Aspose.Words for Java</Product></Products><EditionType>Enterprise</EditionType><SubscriptionExpiry>20991231</SubscriptionExpiry><LicenseExpiry>20991231</LicenseExpiry><SerialNumber>8bfe198c-7f0c-4ef8-8ff0-acc3237bf0d7</SerialNumber></Data><Signature>sNLLKGMUdF0r8O1kKilWAGdgfs2BvJb/2Xp8p5iuDVfZXmhppo+d0Ran1P9TKdjV4ABwAgKXxJ3jcQTqE/2IRfqwnPf8itN8aFZlV3TJPYeD3yWE7IT55Gz6EijUpC7aKeoohTb4w2fpox58wWoF3SNp6sK6jDfiAUGEHYJ9pjU=</Signature></License>";
+            ByteArrayInputStream is = new ByteArrayInputStream(s.getBytes());
+            License license = new License();
+            license.setLicense(is);
+            com.aspose.words.Document document = new com.aspose.words.Document(docPath);
+            document.save(new FileOutputStream(new File(savePath)),SaveFormat.PDF);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
